f5b0df10e88c66dc8986a6b0af1751d032dce770
Yuanhao Wei <yuanhao1@cs.cmu.edu>
2017-11-29 10:12:47 -0500

    renamed compile and run scripts

---
+++ run_universal_benchmark.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd builds
+
+cd libcuckoo
+./tests/universal-benchmark/universal_benchmark --reads 100 --prefill 75 --total-ops 500 --initial-capacity 23
+./tests/universal-benchmark/universal_benchmark --upserts 100 --prefill 25 --total-ops 200 --initial-capacity 23
+cd ..
+
+cd dhash
+./tests/universal-benchmark/universal_benchmark --reads 100 --prefill 75 --total-ops 500 --initial-capacity 23
+./tests/universal-benchmark/universal_benchmark --upserts 100 --prefill 25 --total-ops 200 --initial-capacity 23
+cd ..
+	
+cd ndhash
+./tests/universal-benchmark/universal_benchmark --reads 100 --prefill 75 --total-ops 500 --initial-capacity 23
+./tests/universal-benchmark/universal_benchmark --upserts 100 --prefill 25 --total-ops 200 --initial-capacity 23
+cd ..
+
+cd folklore
+./tests/universal-benchmark/universal_benchmark --reads 100 --prefill 75 --total-ops 500 --initial-capacity 23
+./tests/universal-benchmark/universal_benchmark --upserts 100 --prefill 25 --total-ops 200 --initial-capacity 23
+cd ..
+
+cd ..
