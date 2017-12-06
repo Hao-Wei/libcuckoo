f92e9529732c9e632de55b6208afba22d38fbc0b
Yuanhao Wei <yuanhao1@cs.cmu.edu>
2017-12-06 12:32:41 -0500

    added analysis script

--- analyse_results.py
+++ analyse_results.py
@@ -1,7 +1,38 @@
 from pprint import pprint
+import os
 
 if __name__ == '__main__':
-	s = open('results/2017-12-02_17:56:11/folklore_1_threads_100_0_0', 'r').read()
-	s = eval(s)
-	pprint(s)
+	ll = os.listdir("results")
+	ll.sort()
+	result_dir = 'results/' + max(ll) + '/'
+	graph_dir = 'graphs/' + max(ll) + '/'
+	if(not os.path.isdir("graphs")):
+		os.mkdir("graphs")
+	if(not os.path.isdir(graph_dir)):
+		os.mkdir(graph_dir)
+	
+	d = {}
+	
+	for f in os.listdir(result_dir):
+		s = open(result_dir + f, 'r').read()
+		s = eval(s)
+		table = s['table']
+		throughput = s['output']['throughput']
+		runtime = s['output']['time_elapsed']
+		total_ops = s['output']['total_ops']
+		
+		ss = s['args'].split(' ')
+		args = {}
+		for i in range(0, len(ss), 2):
+			args[ss[i][2:]] = ss[i+1]
+		
+		test = table + '-' + args['reads'] + '-' + args['inserts'] + '-' + args['upserts']
+		if test not in d:
+			d[test] = {}
+		d[test][args['num-threads']] = {'throughput': throughput, 'runtime': runtime, 'total_ops': total_ops}
+	
+	pprint(d)
+		
+	
+	
 	
