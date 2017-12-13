ce614e05d2fb82759cd230d14937d0592e62bc42
Yuanhao Wei <yuanhao1@cs.cmu.edu>
2017-12-13 09:58:45 -0500

    asdf

--- run_hashtable_tests.py
+++ run_hashtable_tests.py
@@ -23,7 +23,7 @@ def runTest(table, thread, total_ops=100, initial_capacity=23, prefill=25, reads
 	upserts = str(upserts)
 	
 	program = "./builds/" + table + "/tests/universal-benchmark/universal_benchmark"
-	output_file = "results/"+timestamp+"/"+table+"_"+thread+"_threads_"+reads+"_"+inserts+"_"+upserts
+	output_file = "results-4/"+timestamp+"/"+table+"_"+thread+"_threads_"+reads+"_"+inserts+"_"+upserts
 	params = program + " --reads " + reads + " --inserts " + inserts + " --upserts " + upserts + " --initial-capacity " + initial_capacity + " --prefill " + prefill + " --total-ops " + total_ops + " --num-threads " + thread + " > " + output_file
 	
 	print(params)
@@ -33,22 +33,21 @@ def runTest(table, thread, total_ops=100, initial_capacity=23, prefill=25, reads
 
 if __name__ == '__main__':
 	timestamp = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
-	if(not os.path.isdir("results")):
-		os.mkdir("results")
-	os.mkdir("results/"+timestamp)
+	if(not os.path.isdir("results-4")):
+		os.mkdir("results-4")
+	os.mkdir("results-4/"+timestamp)
 	
 	n_threads = multiprocessing.cpu_count()
 	n_points = 3
-	#tables = ["folklore", "libcuckoo", "ndhash", "random", "paralleldp", "ndquad", "hopscotch"]
-	tables = ["random", "random2", "empty"]
+	tables = ["folklore", "libcuckoo", "ndhash", "random", "paralleldp", "ndquad", "hopscotch"]
 	threads = [int(float(i)/n_points*(n_threads-1))+1 for i in range(n_points+1)]
 	print threads
 	
 	for table in tables:
 		for thread in threads:
-			runTest(table, thread, total_ops=200, initial_capacity=23, prefill=75, reads=100)
+			runTest(table, thread, total_ops=100, initial_capacity=24, prefill=75, reads=100)
 			runTest(table, thread, total_ops=50, initial_capacity=24, prefill=25, inserts=100)
-			runTest(table, thread, total_ops=200, initial_capacity=24, prefill=50, reads=90, inserts=10)
+			runTest(table, thread, total_ops=100, initial_capacity=24, prefill=50, reads = 90, inserts=10)
 			
 #		os.chdir("builds/" + table)
 #		for filename in os.listdir("."):
